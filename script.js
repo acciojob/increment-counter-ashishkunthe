e0c3a5fa075c97de168e0f382632bd079a169c4a
Ashish Kunthe <160906505+ashishkunthe@users.noreply.github.com>
2026-02-16 11:48:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const btn=document.getElementById("button")
+let count=0
+btn.addEventListener("click",()=>{
+	alert(count)
+	document.getElementById("counter").innerText=count
+	count++
+}) 
+
